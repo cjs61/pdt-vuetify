0f178738087edc2d9a5c5bccc57c0a1553736435
Unknown <barre.js@gmail.com>
2018-09-17 19:54:08 +0200

    Appel des produits
    pour ne pas avoir à recopier du code je ne place pas directement ma méthode dans mounted (qui correspond à ajouté à la page) mais dans 
    methods: {  load() {
    ainsi je peux réactualiser une liste de produits sans avoir à dupliquer du code mais juste en appelant la methode load()
    
    problème avec console log... et eslint...

--- gestion-produits/src/components/Products.vue
+++ gestion-produits/src/components/Products.vue
@@ -3,9 +3,20 @@
 </template>
 
 <script>
+import API from '@/lib/API';
 export default {
-    
-}
+    mounted() {
+        this.load();
+    },
+    methods: {
+        load() {
+            API.getProducts()
+            .then(products => {
+                console.log(products);
+            });
+        },
+    },
+};
 </script>
 
 <style>
