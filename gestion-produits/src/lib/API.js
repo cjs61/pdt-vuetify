4d3a6693697bed1ecf758e567d9048a1faf60ffd
Unknown <barre.js@gmail.com>
2018-09-18 12:22:41 +0200

    Essai de correction

--- gestion-produits/src/lib/API.js
+++ gestion-produits/src/lib/API.js
@@ -1,9 +1,11 @@
-import ezFetch from 'ez-fetch';
+//import ezFetch from 'ez-fetch';
 
 const API_URL = 'http://localhost:3000/api/v1/products';
 
 export default {
     getProducts() {
-        return ezFetch(API_URL);
+        //return ezFetch(API_URL);
+        return fetch(API_URL)
+        .then(response => response.json);
     },
 };
