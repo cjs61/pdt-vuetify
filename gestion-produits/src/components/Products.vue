5edf3be549e56017c34399ee8f1232836decf320
Unknown <barre.js@gmail.com>
2018-09-23 17:48:22 +0200

    lien avec products.js
    je glisse products.js dans lib, le tableau s'appelle my_products  :
    dans api, j'importe './products.js' et je retourn my products
    je change ma méthode load dans products.vue

--- gestion-produits/src/components/Products.vue
+++ gestion-produits/src/components/Products.vue
@@ -14,14 +14,14 @@
 
         <v-card-title primary-title>
           <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <h3 class="headline mb-0">{{ product.title }}Kangaroo Valley Safari</h3>
             <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
           </div>
         </v-card-title>
 
         <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
+          <v-btn flat color="orange">See</v-btn>
+         
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -36,11 +36,6 @@
         data() {
             return { 
             products: [
-              {},
-              {},
-              {},
-              {},
-
             ],
             };
     },
@@ -49,8 +44,9 @@
         },
         methods: {
             load() {
-                API.getProducts()
-                .then(products => this.products = products);
+                this.products = API.getProducts();
+                // .then(products => this.products = products);
+            
             },
         },
     };
