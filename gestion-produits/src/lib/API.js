d50af27fc5962e95e84e10a7f6c8a7901d9ca919
Unknown <barre.js@gmail.com>
2018-09-23 17:48:22 +0200

    lien avec products.js
    je glisse products.js dans lib, le tableau s'appelle my_products  :
    dans api, j'importe './products.js' et je retourn my products
    je change ma méthode load dans products.vue

--- gestion-produits/src/lib/API.js
+++ gestion-produits/src/lib/API.js
@@ -1,11 +1,13 @@
 //import ezFetch from 'ez-fetch';
+import './products.js';
 
-const API_URL = 'http://localhost:3000/api/v1/products';
+// const API_URL = 'http://localhost:3000/api/v1/products';
 
 export default {
     getProducts() {
         // return ezFetch(API_URL);
-        return fetch(API_URL)
-        .then(response => response.json);
+        return my_products;
+        // fetch(API_URL)
+        // .then(response => response.json);
     },
 };
