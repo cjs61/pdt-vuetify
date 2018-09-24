91d1cc6b7542efec819dc393697724bbb59a7aff
Unknown <barre.js@gmail.com>
2018-09-24 11:40:39 +0200

    création de la page produit
    je prends le même v-flex que pour products mais j'agrandi la taille.
    je crée ma route 
    j'ajoute :to="... à mon bouton

--- gestion-produits/src/components/Products.vue
+++ gestion-produits/src/components/Products.vue
@@ -1,28 +1,31 @@
 <template lang="html">
-     
-     <v-layout row wrap>
+  <v-layout row wrap>
     <v-flex xs4 v-for="product in products" :key="product.id">
       <v-card >
-      <!-- <v-img
+        <!--<v-img
           src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-          aspect-ratio="2.75"
-        ></v-img>
+          aspect-ratio="2.75">
+          </v-img>
         -->
-
-         <v-card-media :src="product.image" height="200px">
+        <v-card-media :src="product.image" height="400px">
         </v-card-media>
-        
-
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">{{ product.title }}</h3>
-            <div>Description :<br> {{ product.escription }}</div>
+              <div>Description :<br> {{ product.escription }}</div>
+              </div>
           </div>
         </v-card-title>
-
         <v-card-actions>
-          <v-btn flat color="orange">See</v-btn>
-         
+          <v-btn 
+          :to="{
+            name: 'Product',
+            params: {
+              id: product.id
+              }
+              }"
+              flat
+              color="orange">View</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
